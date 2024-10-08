8d5970ae911156c9bfd4d02c3c78281b97e9ad93
firstcyborg <derkaoui010amine@gmail.com>
2024-10-08 17:05:03 +0200

    [Amine] neu Bullshit

--- Testt/src/main/java/org/example/Amine.java
+++ Testt/src/main/java/org/example/Amine.java
@@ -1,4 +1,27 @@
 package org.example;
 
 public class Amine {
+    boolean b;
+    String string;
+
+    public Amine(boolean b, String string) {
+        this.b = b;
+        this.string = string;
+    }
+
+    public boolean isB() {
+        return b;
+    }
+
+    public void setB(boolean b) {
+        this.b = b;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
 }

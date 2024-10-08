307e2241617efffdd9e818f5f55401be3d60d685
firstcyborg <derkaoui010amine@gmail.com>
2024-10-08 16:34:52 +0200

    [Siamak] getters and setters.

--- Testt/src/main/java/org/example/Siamak.java
+++ Testt/src/main/java/org/example/Siamak.java
@@ -1,4 +1,25 @@
 package org.example;
 
 public class Siamak {
+    int num;
+    String name;
+
+    public Siamak(int num, String name) {
+        this.num = num;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getNum() {
+        return num;
+    }
+    public void setNum(int num) {
+        this.num = num;
+    }
 }
